2941ab79a5d3d83b22b91569e20acc6190710bfa
Retorillo <retorillo@outlook.com>
2019-05-23 06:05:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Flip-Calc
+
+Minimal calculator to determine x-coordinates on horizontal flipping.
+Of course, if assumes width is height, usable for vertical flip.
+
+## Install
+
+```bash
+npm insatll -g https://github.com/retorillo/flip-calc.git
+```
+
+## Usage
+
+```bash
+$ flip-calc -w 30 1.23 5.67
+1.23 => 28.77
+5.67 => 24.33
+```
+
+- Use `-w` (or `--width`) to specify canvas width
+
+## License
+
+MIT
+
+(C) 2019 Retorillo
---
+++ bin/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const gnuopt = require('gnu-option');
+
+const optmap = {
+  'w':  '&width',
+  'width': 'integer',
+};
+
+const opt = gnuopt.parse(optmap);
+
+if (!opt.hasOwnProperty('width')) {
+  throw new Error("--width option is mandatory");
+}
+const w = opt.width;
+
+for (const arg of opt.$) {
+  try {
+    const f = parseFloat(arg);
+    const offset = f - (w / 2);
+    console.log(`${arg} => ${(w / 2) - offset}`);
+  }
+  catch (e) {
+    console.log('abc');
+  }
+}
+
